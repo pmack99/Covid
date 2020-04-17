7451d5b78234b3b53593bcefcb7d509e62a7d7b3
PMack2 <Prestonmack@gmail.com>
2020-04-17 12:24:59 -0400

    added second state card

--- statesTest.js
+++ statesTest.js
@@ -10,6 +10,17 @@ let death = document.getElementById('death')
 let totalTestResults = document.getElementById('totalTestResults')
 let lastUpdateEt = document.getElementById('lastUpdateEt')
 
+let state2 = document.getElementById('state2')
+let positive2 = document.getElementById('positive2')
+let negative2 = document.getElementById('negative2')
+let hospitalizedCurrently2 = document.getElementById('hospitalizedCurrently2')
+let recovered2 = document.getElementById('recovered2')
+let inIcuCurrently2 = document.getElementById('inIcuCurrently2')
+let onVentilatorCurrently2 = document.getElementById('onVentilatorCurrently2')
+let death2 = document.getElementById('death2')
+let totalTestResults2 = document.getElementById('totalTestResults2')
+let lastUpdateEt2 = document.getElementById('lastUpdateEt2')
+
 var selectedState
 
 myFunction()
@@ -59,16 +70,16 @@ function myFunction2() {
       response.json().then(data => {
         console.log(data)
 
-        state.innerHTML = data.state
-        positive.innerHTML = data.positive
-        negative.innerHTML = data.negative
-        hospitalizedCurrently.innerHTML = data.hospitalizedCurrently
-        recovered.innerHTML = data.recovered
-        inIcuCurrently.innerHTML = data.inIcuCurrently
-        onVentilatorCurrently.innerHTML = data.onVentilatorCurrently
-        totalTestResults.innerHTML = data.totalTestResults
-        death.innerHTML = data.death
-        lastUpdateEt.innerHTML = data.lastUpdateEt
+        state2.innerHTML = data.state
+        positive2.innerHTML = data.positive
+        negative2.innerHTML = data.negative
+        hospitalizedCurrently2.innerHTML = data.hospitalizedCurrently
+        recovered2.innerHTML = data.recovered
+        inIcuCurrently2.innerHTML = data.inIcuCurrently
+        onVentilatorCurrently2.innerHTML = data.onVentilatorCurrently
+        totalTestResults2.innerHTML = data.totalTestResults
+        death2.innerHTML = data.death
+        lastUpdateEt2.innerHTML = data.lastUpdateEt
       }),
     )
     .catch(err => {
