5fa54e988b9b8769a663b0cca21215745f36ea7f
PMack2 <Prestonmack@gmail.com>
2020-04-17 12:19:07 -0400

    adding states selectors

--- statesTest.js
+++ statesTest.js
@@ -4,7 +4,7 @@ let positive = document.getElementById('positive')
 let negative = document.getElementById('negative')
 let hospitalizedCurrently = document.getElementById('hospitalizedCurrently')
 let recovered = document.getElementById('recovered')
-let inIcuCumulative = document.getElementById('inIcuCumulative')
+let inIcuCurrently = document.getElementById('inIcuCurrently')
 let onVentilatorCurrently = document.getElementById('onVentilatorCurrently')
 let death = document.getElementById('death')
 let totalTestResults = document.getElementById('totalTestResults')
@@ -18,7 +18,7 @@ function myFunction() {
   var e = document.getElementById('selectState')
   var selectedState = e.options[e.selectedIndex].value
 
-  console.log(selectedState)
+  //   console.log(selectedState)
 
   //Fetching the County Data
   fetch('https://covidtracking.com//api/states?state=' + selectedState, {
@@ -33,7 +33,38 @@ function myFunction() {
         negative.innerHTML = data.negative
         hospitalizedCurrently.innerHTML = data.hospitalizedCurrently
         recovered.innerHTML = data.recovered
-        inIcuCumulative.innerHTML = data.inIcuCumulative
+        inIcuCurrently.innerHTML = data.inIcuCurrently
+        onVentilatorCurrently.innerHTML = data.onVentilatorCurrently
+        totalTestResults.innerHTML = data.totalTestResults
+        death.innerHTML = data.death
+        lastUpdateEt.innerHTML = data.lastUpdateEt
+      }),
+    )
+    .catch(err => {
+      console.log(err)
+    })
+}
+
+function myFunction2() {
+  var e = document.getElementById('selectState2')
+  var selectedState2 = e.options[e.selectedIndex].value
+
+  //   console.log(selectedState)
+
+  //Fetching the County Data
+  fetch('https://covidtracking.com//api/states?state=' + selectedState2, {
+    method: 'GET',
+  })
+    .then(response =>
+      response.json().then(data => {
+        console.log(data)
+
+        state.innerHTML = data.state
+        positive.innerHTML = data.positive
+        negative.innerHTML = data.negative
+        hospitalizedCurrently.innerHTML = data.hospitalizedCurrently
+        recovered.innerHTML = data.recovered
+        inIcuCurrently.innerHTML = data.inIcuCurrently
         onVentilatorCurrently.innerHTML = data.onVentilatorCurrently
         totalTestResults.innerHTML = data.totalTestResults
         death.innerHTML = data.death
