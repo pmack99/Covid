08465d8f3ab9c4133193dd3b45a203533ec959f5
PMack2 <Prestonmack@gmail.com>
2020-04-24 19:20:04 -0400

    trying to add another panel to compare states

--- statesMOD.js
+++ statesMOD.js
@@ -10,21 +10,44 @@ let death = document.getElementById('death')
 let totalTestResults = document.getElementById('totalTestResults')
 let lastUpdateEt = document.getElementById('lastUpdateEt')
 
+let state2 = document.getElementById('state2')
+let positive2 = document.getElementById('positive2')
+let negative2 = document.getElementById('negative2')
+let hospitalizedCurrently2 = document.getElementById('hospitalizedCurrently2')
+let recovered2 = document.getElementById('recovered2')
+let inIcuCurrently2 = document.getElementById('inIcuCurrently2')
+let onVentilatorCurrently2 = document.getElementById('onVentilatorCurrently2')
+let death2 = document.getElementById('death2')
+let totalTestResults2 = document.getElementById('totalTestResults2')
+let lastUpdateEt2 = document.getElementById('lastUpdateEt2')
+
 var selectedState
+var selectedState2
+var pick
+var pick2
+
+let dropdown = document.getElementById('state')
+dropdown.length = 0
+
+let defaultOption = document.createElement('option')
+defaultOption.text = 'Choose State/Province'
+
+dropdown.add(defaultOption)
+dropdown.selectedIndex = 0
+
+let dropdown2 = document.getElementById('state2')
+dropdown2.length = 0
+
+let defaultOption2 = document.createElement('option')
+defaultOption2.text = 'Choose State/Province'
+
+dropdown2.add(defaultOption2)
+dropdown2.selectedIndex = 0
 
 start()
+start2()
 
-// sets up the dropdown
 function start() {
-  let dropdown = document.getElementById('state')
-  dropdown.length = 0
-
-  let defaultOption = document.createElement('option')
-  defaultOption.text = 'Choose State/Province'
-
-  dropdown.add(defaultOption)
-  dropdown.selectedIndex = 0
-
   const url = 'https://covidtracking.com/api/v1/states/info.json'
 
   fetch(url)
@@ -79,33 +102,57 @@ function myFunction() {
   start()
 }
 
-// function myFunction2() {
-//   var e = document.getElementById('selectState2')
-//   var selectedState2 = e.options[e.selectedIndex].value
+function start2() {
+  const url = 'https://covidtracking.com/api/v1/states/info.json'
 
-//   //   console.log(selectedState)
+  fetch(url)
+    .then(function (response) {
+      // Examine the text in the response
+      response.json().then(function (data) {
+        let option
+        var selectedState2 = data
+        console.log(data)
 
-//   //Fetching the County Data
-//   fetch('https://covidtracking.com//api/states?state=' + selectedState2, {
-//     method: 'GET',
-//   })
-//     .then((response) =>
-//       response.json().then((data) => {
-//         // console.log(data)
+        for (let i = 0; i < selectedState2.length; i++) {
+          option = document.createElement('option')
+          option.text = selectedState2[i].name
+          option.value = selectedState2[i].state
+          dropdown.add(option)
+        }
+      })
+    })
+    .catch(function (err) {
+      console.error('Fetch Error -', err)
+    })
+}
 
-//         state2.innerHTML = data.state
-//         positive2.innerHTML = data.positive
-//         negative2.innerHTML = data.negative
-//         hospitalizedCurrently2.innerHTML = data.hospitalizedCurrently
-//         recovered2.innerHTML = data.recovered
-//         inIcuCurrently2.innerHTML = data.inIcuCurrently
-//         onVentilatorCurrently2.innerHTML = data.onVentilatorCurrently
-//         totalTestResults2.innerHTML = data.totalTestResults
-//         death2.innerHTML = data.death
-//         lastUpdateEt2.innerHTML = data.lastUpdateEt
-//       }),
-//     )
-//     .catch((err) => {
-//       console.log(err)
-//     })
-// }
+function myFunction2() {
+  var e2 = document.getElementById('state2')
+  var pick2 = e2.options[e2.selectedIndex].value
+
+  //Fetching the County Data
+  fetch('https://covidtracking.com//api/states?state=' + pick2, {
+    method: 'GET',
+  })
+    .then((response) =>
+      response.json().then((data) => {
+        console.log(data)
+
+        state2.innerHTML = data.state
+        positive2.innerHTML = data.positive
+        negative2.innerHTML = data.negative
+        hospitalizedCurrently2.innerHTML = data.hospitalizedCurrently
+        recovered2.innerHTML = data.recovered
+        inIcuCurrently2.innerHTML = data.inIcuCurrently
+        onVentilatorCurrently.innerHTML = data.onVentilatorCurrently
+        totalTestResults2.innerHTML = data.totalTestResults
+        death2.innerHTML = data.death
+        lastUpdateEt2.innerHTML = data.lastUpdateEt
+      }),
+    )
+    .catch((err) => {
+      console.log(err)
+    })
+
+  start2()
+}
